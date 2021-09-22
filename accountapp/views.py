833318319cae2675988b6124b9ef1ad02d13c9e4
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2021-09-23 00:24:11 +0900

    0.01

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'base.html')
