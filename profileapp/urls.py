3d78770f77fcd8ef256c16c8c047966ab817edf9
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2021-10-01 00:56:13 +0900

    0.09

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -5,4 +5,5 @@ app_name='profileapp'
 
 urlpatterns = [
     path('create/', views.Profile_Create_View.as_view(), name='create'),
+    path('update/<int:pk>', views.Profile_Update_View.as_view(), name='update'),
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.urls.base import reverse
+from django.views.generic import CreateView, UpdateView
+
+from profileapp.decorators import profile_ownership_required
 from .models import Profile
 from .forms import Profile_Creation_Form
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 
 class Profile_Create_View(CreateView):
     model = Profile
-    context_object_name = 'targetProfile'
+    context_object_name = 'target_profile'
     form_class = Profile_Creation_Form
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -17,3 +21,17 @@ class Profile_Create_View(CreateView):
         temp_profile.save()
 
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class Profile_Update_View(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = Profile_Creation_Form
+    template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
